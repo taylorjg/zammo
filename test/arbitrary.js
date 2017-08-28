8c66b3effac7dbd683915d58bb7994dd1736231c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-08-28 22:59:19 +0100

    shrinkIntegral not quite working yet

--- test/arbitrary.js
+++ test/arbitrary.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { shrinkList } from '../lib';
+import { shrinkList, shrinkIntegral } from '../lib';
 
 describe('arbitrary', () => {
 
@@ -7,7 +7,13 @@ describe('arbitrary', () => {
         const xs = [1, 2, 3];
         const shr = () => [];
         const actual = shrinkList(shr, xs);
-        const expected = [[],[2,3],[1,3],[1,2]];
+        const expected = [[], [2, 3], [1, 3], [1, 2]];
+        expect(actual).to.deep.equal(expected);
+    });
+
+    it('shrinkIntegral', () => {
+        const actual = shrinkIntegral(42);
+        const expected = [0, 21, 32, 37, 40, 41];
         expect(actual).to.deep.equal(expected);
     });
 });
